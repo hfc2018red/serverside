ec63ade0a50376522e0c3c6ea311145d23643b33
Mike Lay <mike@mkly.io>
2018-10-20 09:45:42 -0700

    Rip

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const passport = require('passport');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const LocalStrategy = require('passport-local').Strategy;
-const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 
@@ -13,38 +8,17 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 io.set('origins', '*:*');
 
-const auth = (req, res, next) => {
-  console.log(req.path);
-  if (!req.user && req.path === '/') {
-    res.redirect('login.html');
-  }
-
-  next();
-};
-
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(session({ secret: 'test' }));
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 
-const User = require('./models/User');
+//const User = require('./models/User');
 
-passport.use(new LocalStrategy(User.authenticate()));
+require('./routes')(app, io)
+require('./auth')(app);
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-require('./routes')(app, io, auth);
-require('./auth')(app, auth);
-
-app.use(auth);
 app.use(express.static('serverpublic'));
 app.use(express.static('build'));
 
