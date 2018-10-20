0439b755797e8d20f3f9c26f1af3a05bbb6b75a8
Mike Lay <mike@mkly.io>
2018-10-20 01:42:01 -0700

    Add cors

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ const LocalStrategy = require('passport-local').Strategy;
 
 const port = process.env.PORT || 3000;
 
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+}
 const app = express();
+app.use(allowCrossDomain);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
