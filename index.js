780c31ebdd1787895c0553a2b8efae2d66dda31e
Mike Lay <mike@mkly.io>
2018-10-20 03:24:46 -0700

    Adjust cors

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const port = process.env.PORT || 3000;
 
 var allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
   next();
 }
