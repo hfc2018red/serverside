7dc1bcfeade5f24a40e99d2b02f24894fdefea94
Mike Lay <mike@mkly.io>
2018-10-20 03:28:39 -0700

    Cors

--- index.js
+++ index.js
@@ -4,16 +4,12 @@ const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const LocalStrategy = require('passport-local').Strategy;
+const = require('cors');
 
 const port = process.env.PORT || 3000;
 
-var allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-
-  next();
-}
 const app = express();
-app.use(allowCrossDomain);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
