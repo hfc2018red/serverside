6bf22151710de380b1b80869c02d42464af4228b
Mike Lay <mike@mkly.io>
2018-10-20 05:32:32 -0700

    Cors

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-io.set('origins', 'localhost:3000');
+// io.set('origins', '*:*');
 
 app.use(cors());
 app.use('/admin', proxy(proxyOptions));
